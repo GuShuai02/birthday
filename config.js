c08f52f6c647672d638bafb008ef6b183b950125
GuShuai02 <102378113+GuShuai02@users.noreply.github.com>
2022-12-02 21:19:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ var config = {
         "今年要吃好喝好天天开心呀",
         "把我家可爱猪猪喂饱饱",
         "然后抱回家~",
-        "\^3^/",
+        "^3^",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,7 +25,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "最爱的大宝贝": "./imgs/1.jpg",
+        "最爱的大宝贝": "./imgs/pic.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -34,9 +34,9 @@ var config = {
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        cake_fadein: "蛋糕~",
+        light_candle: "蜡烛~",
+        wish_message: "大宝贝生日快乐~",
         story: "A MESSAGE FOR YOU",
     }
 };
